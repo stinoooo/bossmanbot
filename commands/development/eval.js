998129e7e221622804edbfbc28373e65ca1050d1
stino <stabbajabba@gmail.com>
2024-10-10 12:44:02 +0200

    Add files via upload

--- commands/development/eval.js
+++ commands/development/eval.js
@@ -1,19 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName('eval')
-      .setDescription('Executes arbitrary JavaScript code.')
-      .addStringOption(option => option.setName('code').setDescription('Code to execute').setRequired(true)),
-  
-    async execute(interaction) {
-      if (interaction.user.id !== 'YOUR_ID') return interaction.reply('You do not have permission to run this command.');
-  
-      const code = interaction.options.getString('code');
-      try {
-        const result = eval(code);
-        await interaction.reply(`Result: ${result}`);
-      } catch (error) {
-        await interaction.reply(`Error: ${error.message}`);
-      }
+  data: new SlashCommandBuilder()
+    .setName('eval')
+    .setDescription('Executes arbitrary JavaScript code.')
+    .addStringOption(option => 
+      option.setName('code')
+        .setDescription('Code to execute')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    // Replace 'YOUR_ID' with your actual Discord user ID
+    if (interaction.user.id !== '186117507554344960') {
+      return interaction.reply('You do not have permission to run this command.');
     }
-  };
-  
+
+    const code = interaction.options.getString('code');
+    try {
+      const result = eval(code);
+      await interaction.reply(`Result: ${result}`);
+    } catch (error) {
+      await interaction.reply(`Error: ${error.message}`);
+    }
+  }
+};
